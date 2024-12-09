6ccabe1839df18a0c85a8d4de8630638302c8e2e
spumesco <jjhoyun@gmail.com>
2024-12-09 10:39:28 +0900

    Update st_form.py

--- st_form.py
+++ st_form.py
@@ -4,18 +4,18 @@ st.title('st.form')
 
 # 'with' 표기법을 사용한 전체 예시
 st.header('1. `with` 표기법 사용 예시')
-st.subheader('커피 머신')
+st.subheader('토스트 가게')
 
 with st.form('my_form'):
-    st.subheader('**커피 주문하기**')
+    st.subheader('**토스트 주문하기**')
 
     # 입력 위젯
-    coffee_bean_val = st.selectbox('커피콩', ['아라비카', '로부스타'])
-    coffee_roast_val = st.selectbox('커피 로스팅', ['라이트', '미디엄', '다크'])
-    brewing_val = st.selectbox('추출 방법', ['에어로프레스', '드립', '프렌치 프레스', '모카 포트', '사이폰'])
-    serving_type_val = st.selectbox('서빙 형식', ['핫', '아이스', '프라페'])
-    milk_val = st.select_slider('우유 정도', ['없음', '낮음', '중간', '높음'])
-    owncup_val = st.checkbox('자신의 컵 가져오기')
+    coffee_bean_val = st.selectbox('빵의 종류', ['우유식빵', '호밀빵'])
+    coffee_roast_val = st.selectbox('빵의 굽기', ['없음', '적당함', '바삭함'])
+    brewing_val = st.selectbox('토스트 종류', ['에그마요', '베이컨', 'BLT', '햄치즈', '야채'])
+    serving_type_val = st.selectbox('서빙 형식', ['배달', '픽업', '가게 내 취식'])
+    milk_val = st.select_slider('리뷰 이벤트', ['우유', '콜라', '사이다', '아이스티'])
+    owncup_val = st.checkbox('자신의 다회용기 가져오기')
 
     # 모든 양식은 제출 버튼을 가져야 함
     submitted = st.form_submit_button('제출')
@@ -23,19 +23,19 @@ with st.form('my_form'):
 if submitted:
     st.markdown(f'''
         ☕ 주문하신 내용:
-        - 커피콩: `{coffee_bean_val}`
-        - 커피 로스팅: `{coffee_roast_val}`
-        - 추출 방법: `{brewing_val}`
+        - 빵의 종류: `{coffee_bean_val}`
+        - 빵의 굽기: `{coffee_roast_val}`
+        - 토스트 종류: `{brewing_val}`
         - 서빙 형식: `{serving_type_val}`
-        - 우유: `{milk_val}`
-        - 자신의 컵 가져오기: `{owncup_val}`
+        - 이벤트: `{milk_val}`
+        - 자신의 다회용기 가져오기: `{owncup_val}`
         ''')
 else:
     st.write('☝️ 주문하세요!')
 
 
 # 객체 표기법을 사용한 짧은 예시
-st.header('2. 객체 표기법 예시')
+st.header('2. 토스트 개수')
 
 form = st.form('my_form_2')
 selected_val = form.slider('값 선택')
